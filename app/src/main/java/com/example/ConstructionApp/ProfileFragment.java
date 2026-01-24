50964db028a4464f4ff2bea3a5dfda6e6f0d53fd
Earnest18 <reyesearnest08182005@gmail.com>
2026-01-24 23:44:16 +0800

    UPDATED 1/24/26 11:43 PM
    - FIXED LOGOUT
    
    NEED TO BE FIX NEXT:
    
    - DB OF RECENT SEARCH AND RECENT CHAT
    - PROFILE OF USER
    - CREATION OF NOTIFICATIONS
    - WHEN USING BACK FROM PHONE
    - DELETING MESSAGES
    - LIKE, COMMENT, SHARE
    - VALIDATIONS
    - WORKERS PAGE

--- app/src/main/java/com/example/ConstructionApp/ProfileFragment.java
+++ app/src/main/java/com/example/ConstructionApp/ProfileFragment.java
@@ -1,61 +1,59 @@
 package com.example.ConstructionApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private FirebaseFirestore db;
-
-    private String userLocation = "";
-
-    private TextView txtLocation;
+    private Button logout;
 
     public ProfileFragment() {
-
-    }
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = FirebaseFirestore.getInstance();
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-;
+
+        logout = view.findViewById(R.id.logout_btn);
+
+        logout.setOnClickListener(v -> {
+            // 🔹 Sign out
+            FirebaseUtil.logout();
+
+            // 🔹 Redirect to LoginActivity
+            if (getActivity() != null) {
+                Intent intent = new Intent(getActivity(), GetStartedActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                getActivity().finish();
+            }
+
+
+        });
+
         return view;
     }
-
-
 }
